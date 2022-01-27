443ecb49465284251d41b5c547706e1f8274c2a1
SeungMiL <tmskqj@naver.com>
2022-01-28 00:22:02 +0900

    shop_4

--- src/Detail.js
+++ src/Detail.js
@@ -1,35 +1,70 @@
-import React, {usestate} from "react";
+import React, { useEffect, usestate } from "react";
 import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import styled from "styled-components";
+import './detail.scss';
 
 
-function Detail(props){
 
-    let {id} = useParams();
-    let findItem = props.shoes.find(function(item){
+
+let Box = styled.div`
+    padding : 20px;
+`;
+
+let Title = styled.h4`
+    font-size : 25px;
+    color : ${ props => props.colour };
+`;
+
+function Detail(props) {
+
+    useEffect(()=>{
+
+        let alertBox = document.querySelector('.my-alert-yellow');
+        
+        setTimeout(() => {
+            alertBox.classList.add('box-none');
+            alertBox.classList.remove('my-alert-yellow');
+            alertBox.removeChild( alertBox.childNodes[ 0 ] ); 
+            
+        }, 2000);
+        
+    });
+
+    let { id } = useParams();
+    let history = useHistory();
+    let findItem = props.shoes.find(function (item) {
         return item.id == id
     })
 
-    let history = useHistory();
+    
 
-    return(
-      <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+    return (
+        <div className="container">
+
+            <Box>
+                <Title className="red" >Detail</Title>
+            </Box>
+
+            <div className="my-alert-yellow">
+                <p>재고가 얼마 남지 않았습니다</p>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5"> {findItem.title} </h4>
+                    <p>{findItem.content}</p>
+                    <p>{findItem.price}원</p>
+                    <button className="btn btn-danger">주문하기</button>
+                    <button className="btn btn-danger" onClick={() => {
+                        history.goBack();
+                    }}>뒤로가기</button>
+                </div>
+            </div>
         </div>
-        <div className="col-md-6 mt-4">
-          <h4 className="pt-5"> {findItem.title} </h4>
-          <p>{findItem.content}</p>
-          <p>{findItem.price}원</p>
-          <button className="btn btn-danger">주문하기</button>
-          <button className="btn btn-danger" onClick={()=>{
-              history.goBack();
-          }}>뒤로가기</button>
-        </div>
-      </div>
-    </div>
     )
-  }
+}
 
 
-  export default Detail;
+export default Detail;
